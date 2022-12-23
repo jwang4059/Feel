2b7dd95a05a1f77adb8a5e7a6bbbd45260ca6170
jwang0110 <johnw7@uci.edu>
2022-12-23 13:41:50 -0800

    added state and display changes in ui

--- App.tsx
+++ App.tsx
@@ -1,14 +1,39 @@
-import React from "react";
+import React, { useState } from "react";
 import { Button, FlatList, StyleSheet, Text, View } from "react-native";
+import emotions from "./src/data/emotions";
 
 export default function App() {
+	const [selections, setSelections] = useState<string[]>([]);
+
+	let map = emotions;
+	for (const selection of selections) {
+		map = map[selection];
+	}
+
+	const data = map ? Object.keys(map) : null;
+
 	return (
 		<View style={styles.container}>
 			<View>
 				<Text>Title</Text>
 			</View>
 			<View>
-				<FlatList data={[]} renderItem={() => <Button title={"Text"} />} />
+				{data ? (
+					<FlatList
+						data={data}
+						renderItem={({ item }) => (
+							<Button
+								title={item}
+								onPress={() => setSelections([...selections, item])}
+							/>
+						)}
+					/>
+				) : (
+					<Text>{selections.join("")}</Text>
+				)}
+			</View>
+			<View>
+				<Text>Status Bar</Text>
 			</View>
 		</View>
 	);
